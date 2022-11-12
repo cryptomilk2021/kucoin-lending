9b71d1644801c50405137277d5cab09fe2665aa9
nick veniba <cryptomilk2021@gmail.com>
2022-11-13 05:05:35 +1000

    ver 0.1.3

--- main.py
+++ main.py
@@ -10,9 +10,5 @@ for i in my_coin_list:
     lending_coin.lend_coin()
     del lending_coin
 
-##TODO no writing to .csv
 ##TODO *args call to kucoin
 ##TODO only lend when process_open_orders returns an amount?
-#TODO convert precision file to dict as well as not lendable coins
-#TODO find optimal amounts to lend ie. >USDT20, breaks into 0.00ETH, cannot lend ETH even though it's more than USDT10
-#todo BAT has minimum of 10....
