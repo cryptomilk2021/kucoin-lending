93eb193c3d7b022d6f3dfadd01d15592878b7c86
nick veniba <cryptomilk2021@gmail.com>
2022-11-15 09:43:39 +1000

    ver 1.0.0

--- loan.py
+++ loan.py
@@ -5,12 +5,11 @@ import hmac
 import os
 import hashlib
 import pandas as pd
-import ast
 import json
-import csv
 from requests.exceptions import HTTPError
 from datetime import datetime
 import math
+
 
 def write_to_log(*data):
     if data:
@@ -20,6 +19,21 @@ def write_to_log(*data):
     else:
         with open("trans_log.txt", "a") as myfile:
             myfile.write('\n')
+
+
+def format_amount(amount, decimal_places):
+    def truncate(amt, dec_places):
+        return math.floor(amt * 10 ** dec_places) / 10 ** dec_places
+
+    decimal_places = str(decimal_places)
+    chopping = decimal_places.find('.')
+    if chopping == -1:
+        # precision = 1
+        amount = int(float(amount))
+    else:
+        decimal_places = len(decimal_places.replace('.', ' ').split()[-1])
+        amount = truncate(float(amount), decimal_places)
+    return amount
 
 
 class Loan:
@@ -32,17 +46,14 @@ class Loan:
         self.api_secret = None
         self.api_passphrase = None
         self.api_key = None
-        self.api_name = None
         self.open_orders = pd.DataFrame()
         self.coin = coin
         self.precision = None
-        self.min_USDT_amount = 10
         self.delete_orders_older_than_hours = delete_orders_older_than_hours
         self.get_os_vars()
         self.get_currency_precision()
 
     def get_os_vars(self):
-        self.api_name = os.environ.get("API_NAME")
         self.api_key = os.environ.get("API_KEY")
         self.api_passphrase = os.environ.get("API_PASSPHRASE")
         self.api_secret = os.environ.get("API_SECRET")
@@ -51,7 +62,8 @@ class Loan:
 
     def start_log(self):
         write_to_log()
-        write_to_log(f'----------Created {self.coin} instance, deleting orders older than {self.delete_orders_older_than_hours} hours----------')
+        write_to_log(
+            f'----------Created {self.coin} instance, deleting orders older than {self.delete_orders_older_than_hours} hours----------')
 
     def get_lending_rates(self):
         point = f'/api/v1/margin/market?currency={self.coin}&term=7'
@@ -75,7 +87,8 @@ class Loan:
         self.account_balance = pd.DataFrame(response['data'])
         self.account_balance = self.account_balance.loc[self.account_balance['type'] == 'main']
         # self.account_balance.to_csv(f'{self.coin}_account_balance_data.csv')
-        write_to_log(f'{round(float(self.account_balance["available"]), 2)} {self.coin} available for lending, lend precision {self.precision}')
+        write_to_log(
+            f'{round(float(self.account_balance["available"]), 2)} {self.coin} available for lending, lend precision {self.precision}')
         return float(self.account_balance['available'])
 
     def lend_coin(self):
@@ -94,6 +107,7 @@ class Loan:
                 amount = amt / i
                 if amount >= float(precision):
                     return [i, truncate_amt(amount, precision)]
+            write_to_log('cannot lend, not enough assets')
             return None
 
         if self.get_lending_rates():
@@ -105,16 +119,13 @@ class Loan:
                     lend_interest_rate_daily = self.current_lending_rates.iloc[0]['dailyIntRate']
 
                     lend_interest_rate_daily = format(float(lend_interest_rate_daily) + i * 0.00001, '.5f')
-                    # lend_interest_rate_daily = format(lend_interest_rate_daily, '.5f')
 
-                    # print(lend_amount)
                     lend_amount = split_amounts[1]
-                    # print(lend_amount)
-                    # print(type(lend_amount))
                     if lend_amount > 0:
                         order_nbr = self.place_order(lend_amount, lend_interest_rate_daily)
                         if order_nbr.json()['code'] == '400100':
-                            write_to_log(f'Failed Lending {lend_amount} {self.coin}, coin precision was unknown, recorded in min_lend_amount.json for future use')
+                            write_to_log(
+                                f'Failed Lending {lend_amount} {self.coin}, coin precision was unknown, recorded in min_lend_amount.json for future use')
                             write_to_log(order_nbr.json()['code'])
                             write_to_log(order_nbr.json()['msg'])
                             pass
@@ -122,11 +133,8 @@ class Loan:
                             a = order_nbr.json()['data']
                             a = a['orderId']
                             apr = float(lend_interest_rate_daily) * 365 * 100
-                            write_to_log(f'Lending {lend_amount} {self.coin}, @ {lend_interest_rate_daily}% daily ({round(apr, 2)}%APR), 7 days, order {a}')
-                    else:
-                        write_to_log(f'Cannot lend {self.coin}, amount > USDT10 but after conversion {lend_amount} {self.coin}')
-        # else:
-        #     write_to_log(f'unable to lend, only {round(USDT_funds_available, 2)} USDT available')
+                            write_to_log(
+                                f'Lending {lend_amount} {self.coin}, @ {lend_interest_rate_daily}% daily ({round(apr, 2)}%APR), 7 days, order {a}')
 
     def get_last_price(self):
         point = '/api/v1/market/orderbook/level1?symbol=' + self.coin + '-USDT'
@@ -142,16 +150,14 @@ class Loan:
         today = time.time()
         compare_date = (today - time_in_secs) * 1000
 
-        self.get_lending_rates() # happens twice, clean it up
+        self.get_lending_rates()  # happens twice, clean it up
         lend_interest_rate_daily = self.current_lending_rates.iloc[0]['dailyIntRate']
         lowest_rate = format(float(lend_interest_rate_daily), '.5f')
         for index in range(len(df)):
             dict_order = df['items'].loc[index]
-            # print(dict_order['dailyIntRate'], lowest_rate) ####
-            #if order on top of queue, leave it
             if dict_order['dailyIntRate'] == lowest_rate:
-                # print(f'skipping this one')
-                write_to_log(f'not deleting {dict_order["orderId"]}, currently at lowest available rate {lowest_rate}% daily')
+                write_to_log(
+                    f'not deleting {dict_order["size"]} {self.coin}, {dict_order["orderId"]}, currently at lowest available rate {lowest_rate}% daily')
                 continue
             created = float(dict_order['createdAt'])
             if created < compare_date:
@@ -197,19 +203,11 @@ class Loan:
 
     def place_order(self, amount, interest):
         point = '/api/v1/margin/lend'
-        # print(amount)
-        # amount = self.format_amount(amount, self.precision)
-        # print(amount)
-        # print(type(amount))
-        # if amount == 0:
-        #     print('cannot place order')
         data = {"currency": self.coin,
                 "size": amount,
                 "dailyIntRate": interest,
                 "term": 7}
-        # exit()
         data_json = json.dumps(data)
-        # print(data_json)
 
         now = int(time.time() * 1000)
         str_to_sign = str(now) + 'POST' + '/api/v1/margin/lend' + data_json
@@ -227,15 +225,13 @@ class Loan:
             "Content-Type": "application/json"  # specifying content type or using json=data in request
         }
         response = requests.request('post', self.url + point, headers=headers, data=data_json)
-        # print(response)
-        # print(response.json())
+
         if response.json()['code'] == '400100':  # add coin to precision file, check if not another error
             err_msg = response.json()['msg'].split()
             if (err_msg[0] + err_msg[1]) == 'lendsize':
                 # print('lend size error')
                 self.precision = err_msg[5]
             else:
-                # print('found new precision')
                 self.precision = response.json()['msg'].split()[-1]
 
             f = open('min_lend_amount.json')
@@ -249,7 +245,6 @@ class Loan:
         return response
 
     def get_currency_precision(self):
-        # print(f'getting curr precision {self.coin}')
         if not os.path.isfile('min_lend_amount.json'):
             self.precision = 0.1
             dict_lend = {self.coin: self.precision}
@@ -269,22 +264,6 @@ class Loan:
                 with open('min_lend_amount.json', 'w') as convert_file:
                     convert_file.write(json.dumps(dict_lend))
                 write_to_log(f'adding {self.coin} to min_lend_amount.json with default precision of 0.1')
-        # print(f'precision = {self.precision}')
-
-
-    def format_amount(self, amount, decimal_places):
-        def truncate(amt, dec_places):
-            return math.floor(amt * 10 ** dec_places) / 10 ** dec_places
-
-        decimal_places = str(decimal_places)
-        chopping = decimal_places.find('.')
-        if chopping == -1:
-            # precision = 1
-            amount = int(float(amount))
-        else:
-            decimal_places = len(decimal_places.replace('.', ' ').split()[-1])
-            amount = truncate(float(amount), decimal_places)
-        return amount
 
 
 def get_my_coins_list():
@@ -296,7 +275,6 @@ def get_my_coins_list():
     else:
         write_to_log(f'unlendable.csv does not exist, will be created as need be')
 
-    api_name = os.environ.get("API_NAME")
     api_key = os.environ.get("API_KEY")
     api_passphrase = os.environ.get("API_PASSPHRASE")
     api_secret = os.environ.get("API_SECRET")
@@ -320,6 +298,13 @@ def get_my_coins_list():
         "KC-API-KEY-VERSION": "2"
     }
     response = requests.request(call_type, url, headers=headers)
+    if response.status_code == 403:
+        write_to_log()
+        write_to_log(
+            f'HTTP response [{response.status_code}], {response.json()["msg"]}, if IP problem, use IP address associated with your API')
+        write_to_log(f'******************aborting bot******************')
+        write_to_log()
+        exit(1)
 
     my_list = []
     for i in response.json()['data']:
